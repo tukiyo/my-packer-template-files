2b1d5cba8983ac580f2fda0a6dcd017defc70e9b
tukiyo3 <tukiyo3@gmail.com>
2015-07-11 06:55:15 +0900

    最新isoに更新

--- archlinux/iso_download.sh
+++ archlinux/iso_download.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DATETIME=2014.09.03
+DATETIME=2015.07.01
 FILENAME=archlinux-${DATETIME}-dual.iso
 wget -O $FILENAME \
   "http://ftp.tsukuba.wide.ad.jp/Linux/archlinux/iso/${DATETIME}/$FILENAME"
