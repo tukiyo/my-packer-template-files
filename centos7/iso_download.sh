c683c672fde53ef1984831b993f70586804ebd25
tukiyo3 <tukiyo3@gmail.com>
2015-07-16 05:44:13 +0900

    fix

---
+++ centos7/iso_download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+FILENAME=CentOS-7-x86_64-Minimal-1503-01.iso
+wget -O $FILENAME \
+  "http://ftp.tsukuba.wide.ad.jp/Linux/centos/7/isos/x86_64//$FILENAME"
+
--- centos7/scripts/virtualbox.sh
+++ centos7/scripts/virtualbox.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+yum install -y bzip2 gcc
+yum clean all
+
 mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
 /mnt/VBoxLinuxAdditions.run
 umount /mnt
